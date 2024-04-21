751a40d21f1a8ef868769bdce21ede6bca7a3fcd
Parvez-Akhtar-code <parvezakhtar310@gmail.com>
2024-04-21 12:29:53 +0530

    Thread Concept

---
+++ src/MainThread.java
@@ -0,0 +1,13 @@
+public class MainThread {
+    public static void main(String[] args) {
+
+        for (int i=0;i<10;i++){
+            Thread1 thread1 = new Thread1();
+            thread1.start();
+            System.out.println("Main Thread Running");
+
+            Thread thread = new Thread(new Thread2());
+            thread.start();
+        }
+    }
+}
